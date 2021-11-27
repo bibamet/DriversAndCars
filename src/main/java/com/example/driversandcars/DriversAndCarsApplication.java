370e6994258008ba990916b177b8b0fcebb01e43
bibamet <spr1tee1337@gmail.com>
2021-11-27 20:15:12 +0300

    Change database connection

--- src/main/java/com/example/driversandcars/DriversAndCarsApplication.java
+++ src/main/java/com/example/driversandcars/DriversAndCarsApplication.java
@@ -10,7 +10,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class DriversAndCarsApplication {
 
     public static void main(String[] args) {
+
         ConfigurableApplicationContext context = SpringApplication.run(DriversAndCarsApplication.class, args);
+
     }
 
 }
